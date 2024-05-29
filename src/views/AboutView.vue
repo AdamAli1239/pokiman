ff70bed0be13f5a297211cb46adfbc32141b7c79
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-05-28 21:46:50 -0400

    dsfsdf

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,56 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Random pokemon page</h1>
+    <button @click="getID">
+      click to get a random pokemon (you have no say in which)
+    </button>
+    <p>Pokemon name: {{ pokeName }}</p>
+    <p>Pokemon Evolution: {{ pokeEvo }}</p>
+    <p>Pokemon Stats: {{ pokeStats }}</p>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+
+export default {
+  components: {},
+  data() {
+    return {
+      id: null,
+      pokeName: "",
+      pokeStats: "",
+      pokeEvo: "",
+    };
+  },
+  methods: {
+    getID() {
+      this.id = Math.floor(Math.random() * 101);
+
+      this.fetchdapoki().then((data) => {
+        console.log("resolved", data);
+        this.pokeName = data.chain.species.name;
+        console.log(data.chain);
+
+        // console.log(data.chain.species.name);
+        // console.log(data.chain.evolves_to[0].species.name);
+        if (data.chain.evolves_to.length != 0) {
+          this.pokeEvo = data.chain.evolves_to[0].species.name;
+        } else {
+          this.pokeEvo = "No evolution left";
+        }
+      });
+    },
+    async fetchdapoki() {
+      console.log("wagwan");
+
+      const rand = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}/`);
+      const ev = await fetch(
+        `https://pokeapi.co/api/v2/evolution-chain/${this.id}/`
+      );
+      const evsec = await ev.json();
+      const sec = await rand.json();
+      return sec, evsec;
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Pepsi Pokemon thing</h1>
+    <p>
+      I dont really know what else to put here tbh so ima just do random stuff
+      to practice vue skill :3
+    </p>
+    <label>Input stuff into this text it does nothing tho tbh</label>
+    <input type="text" v-model="wlatt" />
+    <p>You inputted - {{ wlatt }}</p>
+    <label>Click for option 1</label>
+    <input type="checkbox" value="hi" v-model="mist" />
+    <label>Click for option 2</label>
+    <input type="checkbox" value="whats" v-model="mist" />
+    <label>Click for option 3</label>
+    <input type="checkbox" value="up" v-model="mist" />
+    <label>Click for option 4</label>
+    <input type="checkbox" value="pepsi" v-model="mist" />
+    <label>Click for option 5</label>
+    <input type="checkbox" value="want some" v-model="mist" />
+    <label>Click for option 6</label>
+    <input type="checkbox" value="water?" v-model="mist" />
+    <p v-for="mis in mist" :key="mis">{{ mis }}</p>
+    <h3>
+      Enter random words I will make them not be able to duplicate, but hold
+      down alt
+    </h3>
+    <input type="text" @keyup.alt="lahlah(e)" v-model="mo" />
+    <p v-for="hi in second" :key="hi">{{ hi }}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  data() {
+    return {
+      mo: "",
+      wlatt: "",
+      mist: [],
+
+      second: [],
+    };
+  },
+  methods: {
+    lahlah(e) {
+      if (this.mo && this.mo[-1] == ",") {
+        console.log("afawd");
+      }
+    },
+  },
+};
 </script>
